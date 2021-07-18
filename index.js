6db84d0c95250a74d0facfa5eaafc2ab7d05dc9c
ArthurHtbk <arthur.heurtebise@gmail.com>
2021-07-18 17:40:26 +0100

    updated

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const formidable = require("express-formidable");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
+const cors = require("cors");
 
 const app = express();
 app.use(formidable());
+app.use(cors());
 
-mongoose.connect("mongodb://localhost/vinted", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
@@ -29,6 +32,6 @@ app.all("*", (req, res) => {
   res.status(404).json({ message: "Page not found" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started!");
 });
